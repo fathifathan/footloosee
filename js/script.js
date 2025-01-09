a51a4fe4b155145de6d237e285b7845c068caf54
Fathi Fathan <88713515+fathifathan@users.noreply.github.com>
2025-01-09 08:50:15 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", () =>{
+  const loader = document.querySelector(".loading-page");
+
+  loader.classList.add("loading-page-hidden");
+
+  loader.addEventListener("transitionend", () => {
+    window.location.href = "index.html";
+  })
+})
+
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.8,
+  }
+);
+
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
